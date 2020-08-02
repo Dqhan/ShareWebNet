340d514012d698706472c66a2f5cbe225e3fac34
dqhan <291927006@qq.com>
2020-08-02 23:58:54 +0800

    d

--- app/CommonWeb/www/webpack.base.js
+++ app/CommonWeb/www/webpack.base.js
@@ -6,7 +6,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     mode: "development",
-    entry: './index.js',
+    entry: path.resolve(__dirname, 'index.js'),
     output: {
         path: path.resolve(__dirname, "./dist/"),
         filename: '[name]-[chunkhash].js',
